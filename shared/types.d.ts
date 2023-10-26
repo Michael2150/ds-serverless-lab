fac0e7a045ff6e7ba4952aa4e3b847ea2cfdd258
Michael Gerber <michaelgerb2150@gmail.com>
2023-10-26 19:57:27 +0100

    Resource to query cast members

--- shared/types.d.ts
+++ shared/types.d.ts
@@ -10,4 +10,17 @@ export type Movie = {
     video: boolean;
     vote_average: number;
     vote_count: number
-  }
+}
+
+export type MovieCast = {
+    movieId: number;
+    actorName: string;
+    roleName: string;
+    roleDescription: string;
+};
+// Used to validate the query string og HTTP Get requests
+export type MovieCastMemberQueryParams = {
+    movieId: string;
+    actorName?: string;
+    roleName?: string
+}
